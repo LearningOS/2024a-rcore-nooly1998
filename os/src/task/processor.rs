585a3cb1a5507b069ddf7fc73847d753e2a8535e
nooly <1090498448@qq.com>
2024-11-07 01:08:18 +0800

    ch5

--- os/src/task/processor.rs
+++ os/src/task/processor.rs
@@ -7,6 +7,8 @@
 use super::__switch;
 use super::{fetch_task, TaskStatus};
 use super::{TaskContext, TaskControlBlock};
+use crate::config::MAX_SYSCALL_NUM;
+use crate::mm::{MapPermission, VirtAddr};
 use crate::sync::UPSafeCell;
 use crate::timer::get_time_ms;
 use crate::trap::TrapContext;
